314cd2ef3b376ed92657b2973505ba074dd6f99d
Shersheial <4740525+sharshi@users.noreply.github.com>
2025-02-04 16:37:23 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import DiffViewer from './components/DiffViewer';
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/diff" element={<DiffViewer />} />
+        </Routes>
+    );
+};
+
+export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export const Home = () => {
+    return (
+        <div>
+            <h1>Diff Viewer</h1>
+            <Link to="/diff">Diff Viewer</Link>
+        </div>
+    );
+};
