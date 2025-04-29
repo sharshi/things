91098d88f517cff1899d2de43c124ab0961ec65c
Shersheial 🕍 <4740525+sharshi@users.noreply.github.com>
2025-04-29 11:09:46 -0400

    Update TaskManager.tsx

--- src/components/TaskManager.tsx
+++ src/components/TaskManager.tsx
@@ -190,7 +190,8 @@ const KanbanPage = () => {
     };
 
     // Show confirmation dialog
-    const showConfirmation = () => {
+    const showConfirmation = (hi: string, func: any) => {
+        func();
     //     const ConfirmDialog = ({ message, onConfirm, onCancel }: any) => (
     //         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-40">
     //             <div className="bg-white p-6 rounded-lg shadow-xl max-w-sm w-full mx-4">
